74676c82ab6740dcb9cb6fbe82356a7a4fbd70c7
Mark M <135105725+MarriottMark@users.noreply.github.com>
2025-05-15 00:11:11 +0000

    did thing

--- main.py
+++ main.py
@@ -2,6 +2,17 @@
 # Create the Book class (plus methods and attributes)
 # Create the Novel class that inherits from Book class.
 # Create the Magazine class that inherits from Book class.
+import customtkinter
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("green")
+
+root = customtkinter.CTk()
+root.geometry("1400x1000")
+
+Font = ("Roboto", 24)
+
+frame = customtkinter.CTkFrame(master=root)
 
 class Book:
     def __init__(self, numpages, year, title):
@@ -9,6 +20,8 @@ class Book:
         self.year = year
         self.title = title
 
+    def rate(self):
+        self.Book += 1
 
 class Novel(Book):
     def __init__(self, genre):
@@ -20,7 +33,14 @@ class Magazine(Book):
         super().__init__(self, self.numpages, self.title, self.year)
 
 
+mag1 = Magazine(12,"Daniel", 445)
+nov1 = Novel(1000000000, "John the book", 2026)
 
 
+compoundbutton = customtkinter.CTkButton(master=root, text="Submit", font=Font, width=50, height=20)
+compoundbutton.pack(pady=16, padx=10)
 
 
+frame.pack(fill="both", expand=True)
+
+root.mainloop()
